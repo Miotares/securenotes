51bb16d4b496310eff1062fce873e19527910635
miotares <miotares@proton.me>
2025-03-17 10:23:38 +0100

    Initial Commit

---
+++ SecureNotes/SecureNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureNotesApp.swift
+//  SecureNotes
+//
+//  Created by Merlin Kreuzkam on 17.03.25.
+//
+
+import SwiftUI
+
+@main
+struct SecureNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
